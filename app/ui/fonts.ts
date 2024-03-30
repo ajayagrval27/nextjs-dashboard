b5d09f79578f359d8133824543359395669764f6
Ajay Agravat <ajayagrval27@gmail.com>
2024-03-30 18:33:25 +0530

    created_project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Poppins } from 'next/font/google';
+
+export const poppins = Poppins({
+  weight: ['500', '700'],
+  subsets: ['latin'],
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
